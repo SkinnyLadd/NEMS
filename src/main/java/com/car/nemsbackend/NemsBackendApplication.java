c839d1cd434aae006287f1d97ca165542b057c53
skinnyladd <aadil.bscs24seecs@seecs.edu.pk>
2025-04-19 20:42:21 +0500

    Init Commit

---
+++ src/main/java/com/car/nemsbackend/NemsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.car.nemsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NemsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/car/nemsbackend/NemsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.nemsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
